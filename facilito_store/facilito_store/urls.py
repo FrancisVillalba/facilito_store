5144d1387eb072b4e8edd631396aacdf70f7b8f3
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-08-10 21:48:49 -0400

    maquetado terminado

--- facilito_store/facilito_store/urls.py
+++ facilito_store/facilito_store/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path 
 
-from facilito_store.views import index,login_vw,logout_vw
+from facilito_store.views import index,login_vw,logout_vw,register_vw
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', login_vw, name='vw-login'),
-    path('logout', logout_vw, name='vw-logout'),
-    path('index', index, name='vw-index'),
+    path('admin/', admin.site.urls), 
+    path('usuarios/login', login_vw, name='vw-login'), 
+    path('', index, name='vw-index'),
+    path('usuarios/logout', logout_vw, name='vw-logout'),
+    path('usuarios/registro', register_vw, name='vw-register'),
 ]
--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth.models import User
 
-def login_vw(request):   
+ 
+
+def login_vw(request):  
+    if request.user.is_authenticated:
+        return redirect('vw-index')
+
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -21,13 +29,40 @@ def login_vw(request):
 
     })
 
+def register_vw(request):  
+    print('HOLAAAAAAAA1')  
+    if request.user.is_authenticated:
+        print('HOLAAAAAAAA')
+        return redirect('vw-index')
+    
+    print('HOLAAAAAAAA2')
+    form = RegisterForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        # username =  form.cleaned_data.get('username')
+        # email =  form.cleaned_data.get('email')
+        # password =  form.cleaned_data.get('password')
+
+        # user = User.objects.create_user(username, email, password)
+        user = form.save()
+        if user:
+            login(request, user)
+            messages.success(request, 'Creado exitosamente')
+
+            return redirect('vw-index')
+
+    return render(request, 'users/register.html',{
+         'form' : form
+     })
+
 def logout_vw(request):   
     logout(request)
 
     messages.success(request, 'Sessión cerrada')
     return redirect('vw-login')
 
-def index(request):
+def index(request): 
+    
     return render(
         request,
         "index.html",

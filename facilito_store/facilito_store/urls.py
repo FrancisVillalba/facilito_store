18014a9229541110d07d2bd2e1ff38ac62ce1e67
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-08-14 14:47:03 -0400

    clase 5 - Clase ListView

--- facilito_store/facilito_store/urls.py
+++ facilito_store/facilito_store/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path 
-
-from facilito_store.views import index,login_vw,logout_vw,register_vw
+from django.urls import path
+from products.views import ProductListView  
+from facilito_store.views import login_vw,logout_vw,register_vw
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('usuarios/login', login_vw, name='vw-login'), 
-    path('', index, name='vw-index'),
+    path('', ProductListView.as_view(), name='vw-index'),
     path('usuarios/logout', logout_vw, name='vw-logout'),
     path('usuarios/registro', register_vw, name='vw-register'),
 ]
--- facilito_store/products/views.py
+++ facilito_store/products/views.py
@@ -1,3 +1,16 @@
+from typing import Any, Dict
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from products.models import Product
 # Create your views here.
+
+class ProductListView(ListView):
+    template_name = 'index.html'
+    queryset = Product.objects.all().order_by('-id')
+
+    def get_context_data(self, **kwargs: Any):
+        context =  super().get_context_data(**kwargs) 
+        context['message'] = 'Listado de productos'
+        
+
+        return context

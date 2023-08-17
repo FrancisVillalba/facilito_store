6b95c39a41aaf6ae1b312842cc65fb7ea7b35575
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-08-17 15:32:25 -0400

    modulo 4 - Respaldo de información

--- facilito_store/products/urls.py
+++ facilito_store/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('search', views.ProductSearchLiestView.as_view(), name='search-view'), 
-    path('<slug:slug>', views.ProductDetailView.as_view(), name='proeuct-view'),  
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-view'),  
 ]
--- facilito_store/products/views.py
+++ facilito_store/products/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from products.models import Product
+from django.db.models import Q
 # Create your views here.
 
 class ProductListView(ListView):
@@ -29,8 +30,9 @@ class ProductSearchLiestView(ListView):
     template_name = "products/search.html"
 
     def get_queryset(self):
+        filters = Q(title__icontains= self.query()) | Q(category__title__icontains = self.query())
         #es como usar like en sql server
-        return Product.objects.filter(title__icontains= self.query())
+        return Product.objects.filter(filters)
     
     def query(self):
         return self.request.GET.get('q')

895675705bd23fa5ddb7bc7248ed28db0bd45a21
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-09-08 09:24:20 -0400

    Modulo 7 - Breadcrumb

--- orders/utils.py
+++ orders/utils.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from .models import Order
 
 
@@ -12,3 +13,12 @@ def get_or_create_order(cart, request):
         request.session['order_id'] = order.order_id
 
     return order
+
+
+def breadcrumb(products=True, addres=False, payment=False, confirmation=False):
+    return[
+        {'title':'Productos', 'active': products, 'url': reverse('orders:order-view')},
+        {'title':'Direccion', 'active': addres, 'url': reverse('orders:order-view')},
+        {'title':'Pago', 'active': payment, 'url': reverse('orders:order-view')},
+        {'title':'Confirmación', 'active': confirmation, 'url': reverse('orders:order-view')},
+    ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from carts.utils import get_or_create_cart 
 from .models import Order
-from .utils import get_or_create_order
+from .utils import get_or_create_order, breadcrumb
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -10,8 +10,10 @@ def order(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
 
+    print(breadcrumb())
 
     return render(request, 'orders/order.html', {
         'cart': cart, 
         'order': order,
+        'breadcrumb': breadcrumb()
     })

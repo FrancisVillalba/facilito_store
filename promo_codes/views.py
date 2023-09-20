992aedaf3ff0147955819a9877571917cb71dd52
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-09-20 14:22:11 -0400

    Modulo 11 - Responder con formato JSON

--- promo_codes/views.py
+++ promo_codes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def validate(request):
+    return JsonResponse({
+        'name': 'Eduardo',
+        'job': 'CodigoFacilito',
+        'courses': [
+            {'title': 'Python'},
+            {'title': 'Java'},
+            {'title': '.NET'}
+        ]
+    })

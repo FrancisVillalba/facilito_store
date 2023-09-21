5277d51ce0369e6b0eaa368f792b64a73bbf22e1
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-09-21 15:25:39 -0400

    Modulo 12 - Crear cliente

--- billing_profiles/views.py
+++ billing_profiles/views.py
@@ -5,7 +5,13 @@ from django.conf import settings
 # Create your views here.
 @login_required(login_url='vw-login')
 def create(request):
-    print("AAAAA")
+     
+    if request.method == 'POST':
+        if request.POST.get('stripeToken'):
+
+            if not request.user.has_customer():
+                request.user.create_customer_id()
+
     return render(request, 'billing_profiles/create.html', {
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY
     })
--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from stripeAPI.customer import create_customer
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,6 +7,8 @@ from orders.common import OrderStatus
 # Create your models here.
 class User(AbstractUser):
 
+    customer_id = models.CharField(max_length=100, blank=True, null=True)
+
     def get_full_name(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
@@ -13,7 +16,19 @@ class User(AbstractUser):
     @property
     def shipping_address(self):
         return self.shippingaddress_set.filter(default=True).first()
+    
+    @property
+    def description(self):
+        return 'Descripción para el usuario {}'.format(self.username)
 
+    def has_customer(self):
+        return self.customer_id is not None
+    
+    def create_customer_id(self):
+        if not self.has_customer(): 
+            customer = create_customer(self)
+            self.customer_id = customer.id
+            self.save()
 
     def has_shipping_address(self):
         return self.shipping_address is not None

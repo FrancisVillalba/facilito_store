bcdaf3997a129e8e65b746a97e77b40c2d99994e
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-09-22 18:12:46 -0400

    Modulo 14 - Paginacion

--- products/views.py
+++ products/views.py
@@ -10,11 +10,13 @@ from django.db.models import Q
 class ProductListView(ListView):
     template_name = 'index.html'
     queryset = Product.objects.all().order_by('-id')
+    paginate_by = 1
 
     def get_context_data(self, **kwargs: Any):
         context =  super().get_context_data(**kwargs) 
         context['message'] = 'Listado de productos'
 
+        print(context)
 
         return context
 class ProductDetailView(DetailView):

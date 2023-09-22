e22eb9242933149224ca362329714c301e43f696
francisco.villalba <francisco.villalba@calimatechnologies.com>
2023-09-22 15:00:28 -0400

    Modulo 12 - Listado de tarjetas

--- billing_profiles/urls.py
+++ billing_profiles/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = 'billing_profiles'
+
 urlpatterns = [ 
+    path('', views.BillingProfileListView.as_view(), name='billing_profiles-view'), 
     path('nuevo', views.create, name='create-view'),  
 ]
--- billing_profiles/views.py
+++ billing_profiles/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib import messages
-
 from .models import BillingProfile
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
+
+class BillingProfileListView(LoginRequiredMixin, ListView):
+    login_url = 'vw-login'
+    template_name = 'billing_profiles/billing_profiles.html'
+
+    def get_queryset(self):
+        return self.request.user.billing_profiles
+
 
 # Create your views here.
 @login_required(login_url='vw-login')
